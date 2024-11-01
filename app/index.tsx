388251ba7b1bec90a4f12d6fc6304583116a8047
marcus <marcus.limtauwhang961@gmail.com>
2024-11-01 23:09:07 +0800

    - add in and setup tailwind and nativewind - add new assets and constants

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 
 const Index = () => {
   return (
-    <View style={styles.container}>
-      <Text>Aora App!</Text>
+    <View 
+        // style={styles.container}
+        className="flex-1 items-center justify-center bg-white"
+    >
+      <Text className="text-3xl font-pblack">Aora App!</Text>
       <StatusBar style='auto' />
       <Link href={"/profile"} style={{color: "blue"}}>Go to Profile</Link>
     </View>
@@ -15,12 +18,12 @@ const Index = () => {
 
 export default Index
 
-const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        backgroundColor: "#fff",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+// const styles = StyleSheet.create({
+//     container: {
+//         display: "flex",
+//         backgroundColor: "#fff",
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center"
+//     }
+// })
